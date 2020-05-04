6eb14c8008a40dc254cc8b6dd9fc465925a39429
KaushalyaMadushani <mkaushalya96@gmail.com>
2020-05-04 14:44:25 +0530

    Implement doPost and doPut methods in PatientAPI class

--- Patient_REST/src/com/caremarque/patient/servlet/PatientAPI.java
+++ Patient_REST/src/com/caremarque/patient/servlet/PatientAPI.java
@@ -1,11 +1,18 @@
 package com.caremarque.patient.servlet;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.caremarque.patient.model.Patient;
+import com.caremarque.patient.service.PatientServiceImpl;
 
 /**
  * Servlet implementation class PatientAPI
@@ -13,6 +20,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/PatientAPI")
 public class PatientAPI extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	PatientServiceImpl patientServiceImpl = new PatientServiceImpl();
 
     /**
      * Default constructor. 
@@ -30,13 +39,76 @@ public class PatientAPI extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
-		
 		doGet(request, response);
+		
+		System.out.println("Patient Api post method");
+		
+		Patient patient = new Patient();
+		
+		patient.setFirstName(request.getParameter("firstName"));
+		patient.setLastName(request.getParameter("lastName"));
+		patient.setGender(request.getParameter("gender"));
+		patient.setNIC(request.getParameter("NIC"));
+		patient.setDOB(request.getParameter("DOB"));
+		patient.setEmail(request.getParameter("email"));
+		patient.setPhone(request.getParameter("phone"));
+		patient.setBloodGroup(request.getParameter("bloodGroup"));
+		patient.setPassword(request.getParameter("password"));
+		patient.setConfirmPassword(request.getParameter("cPassword"));
+		
+		String output = patientServiceImpl.registerPatient(patient);
+		System.out.println(output);
+		response.getWriter().write(output);
 	}
 
+	   private static Map getParasMap(HttpServletRequest request) {
+			
+	    	System.out.println("getParasMap");
+			Map<String, String> map = new HashMap<String, String>();
+			
+			try {
+				
+				Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+				String queryString = scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
+				
+				scanner.close();
+				
+				String[] params = queryString.split("&");
+				
+				for(String param : params) {
+					
+					String[] p = param.split("=");
+					map.put(p[0], p[1]);
+				}					 
+			}catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			return map;
+		}
 	
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+Map paras = getParasMap(request);
+		
+		System.out.println("Patient API put method");
+		System.out.println("patient id: " + paras.get("hidPatientIdSave").toString());
+		System.out.println("patient name: " + paras.get("firstName").toString());
+		
+		String output = patientServiceImpl.updatePatientDetails(
+				paras.get("hidPatientIdSave").toString(),
+				paras.get("firstName").toString(),
+				paras.get("lastName").toString(),
+				paras.get("gender").toString(),
+				paras.get("NIC").toString(),
+				paras.get("DOB").toString(),
+				paras.get("bloodGroup").toString(),
+				paras.get("email").toString(),
+				paras.get("phone").toString(),				
+				paras.get("password").toString(),
+				paras.get("cPassword").toString());
+
+		response.getWriter().write(output);
+		
 	}
 
 	
